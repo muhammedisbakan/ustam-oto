99da845b685912203a7e87ec92ea5710f00d3aec
muhammedisbakan <muhammedisbakan12345@gmail.com>
2026-09-11 16:16:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ustam-oto-v4';
+const CACHE='ustam-oto-v5';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
